f5439b33a175c5b2466824b68220f2a212f98160
emilija-smitaite <emilijasmitaite@yahoo.com>
2022-04-25 14:36:57 +0000

    Fix: tables not being created in the database

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -1,4 +1,4 @@
-from taskmanager import db, Column
+from taskmanager import db
 
 class Category(db.Model):
     # schema for the category model
@@ -19,12 +19,12 @@ class Task(db.Model):
     task_description = db.Column(db.Text, nullable=False)
     is_urgent = db.Column(db.Boolean, default=False, nullable=False)
     due_date = db.Column(db.Date, nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelette="CASCADE"), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
     
 
 
     def __repr__(self):
-        # __repr__ to represent itself in the form of a string
+       # __repr__ to represent itself in the form of a string
        return "#{0} - Task: {1} | Urgent: {2}".format(
             self.id, self.task_name, self.is_urgent
         )
