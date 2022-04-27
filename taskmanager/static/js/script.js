9d02361c574401787125113eb74de56485cfc94a
emilija-smitaite <emilijasmitaite@yahoo.com>
2022-04-27 14:11:22 +0000

    Add functionality to add new categories

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,10 @@
+
+/*document.addEventListener('DOMContentLoaded', function() {
+    var sidenav = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(sidenav);
+  });*/
+  document.addEventListener("DOMContentLoaded", function() {
+    // sidenav initialization
+    let sidenav = document.querySelectorAll(".sidenav");
+    M.Sidenav.init(sidenav);
+});
